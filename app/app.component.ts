dfd637a7be310eae27d973845548d95d6f9c36f2
Mitch Barlow <barlowamitch@gmail.com>
2018-01-26 15:00:57 -0600

    working task card

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {Task} from './model/task';
+
+@Component({
+	moduleId:module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})
+export class AppComponent { 
+	private tasks: Task[] = [];
+	private currentTask = new Task(null,false);
+
+	addTask(){
+		let task = new Task(this.currentTask.content, this.currentTask.completed);
+		this.tasks.push(task);
+		this.currentTask.content = null;
+	}
+}
